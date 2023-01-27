d5065655cfc853f3341e0f508bc7ed8d031fd9aa
Jules Taptso <jules.taptso@psgdover.com>
2023-01-27 11:01:37 +0100

    Test

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// const text = document.getElementById('plainText');
+// const hashText = document.getElementById('hashText');
+
+// const crypto = require('crypto');
+// const hsh = crypto.createHash('sha256');
+
+// console.log(text.textContent);
+
+// hashText.textContent = 'Text hashed';
 
